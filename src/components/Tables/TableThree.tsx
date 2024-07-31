7e583e4faf2745df85ef0cff2a3ab67d77493130
Wuys <lequyvan21203@gmail.com>
2024-07-31 17:19:52 +0700

    test

--- src/components/Tables/TableThree.tsx
+++ src/components/Tables/TableThree.tsx
@@ -133,6 +133,7 @@ const TableThree = () => {
     <div className="  rounded-sm border border-stroke bg-white px-5 pb-2.5 pt-6 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <div className="flex items-center justify-between gap-10">
         <FilterComponent />
+        <h1>Hello World</h1>
       </div>
       <div className="max-w-full overflow-x-auto">
         <table className="w-full table-auto">
